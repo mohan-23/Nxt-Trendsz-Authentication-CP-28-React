eda1e29f921c5071d835357dfd5e34ee4c443a16
mohan-23 <mahankandukoori@gmail.com>
2023-06-23 14:31:27 +0000

    Nxt Trendsz Authentication 2

---
+++ src/components/Cart/index.js
@@ -0,0 +1,18 @@
+import Header from '../Header'
+
+import './index.css'
+
+const Cart = () => (
+  <>
+    <Header />
+    <div className="card-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+        className="card-image"
+        alt="cart"
+      />
+    </div>
+  </>
+)
+
+export default Cart
